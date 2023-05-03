eee3e30bb6719159500e01e882bd86d46b8f0105
anticN <anticn@bzz.ch>
2023-05-03 09:01:24 +0000

    add: /now with ?tz= param solved

--- express_tasks2/index.js
+++ express_tasks2/index.js
@@ -7,6 +7,15 @@ app.get("/", (req, res) =>{
     res.send("This is the root.");
 })
 
+app.get("/now", (req, res) => {
+    let tz = req.query.tz || "Europe/Berlin";
+    res.send(`Current time from the timezone ${tz}: ${new Date().toLocaleTimeString("de-CH", {timeZone: tz})}`);
+})
+
+//app.get("/", (req, res) => {
+
+//})
+
 
 
 app.listen(port, () => {
