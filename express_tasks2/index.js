28ae13be4b73d3ccd403c8b6027775eb6d3fc565
anticN <anticn@bzz.ch>
2023-05-03 12:39:21 +0000

    add name POST and DELETE

--- express_tasks2/index.js
+++ express_tasks2/index.js
@@ -1,7 +1,13 @@
-import express from "express";
+import express, { urlencoded } from "express";
+import cors from "cors";
+import multer from "multer";
 
 const app = express();
 const port = 3000;
+let names = ["Nikola", "Fernando", "Lambou", "Miro"];
+
+app.use(express.urlencoded({extended: true}));
+app.use(cors());
 
 app.get("/", (req, res) =>{
     res.send("This is the root.");
@@ -12,11 +18,22 @@ app.get("/now", (req, res) => {
     res.send(`Current time from the timezone ${tz}: ${new Date().toLocaleTimeString("de-CH", {timeZone: tz})}`);
 })
 
-//app.get("/", (req, res) => {
+app.get("/name", (req, res) => {
+    res.sendFile("/workspaces/m295/express_tasks2/index.html");
+})
 
-//})
+app.post("/names", (req, res) => {
+    let name = req.body.name;
+    names.push(name);
+    res.send(names);
+})
 
-
+app.delete("/delname", multer().none(), (req, res) => {
+    names = names.filter((n) => n !== req.body.name);
+    console.log(req.body.name);
+    console.log(names);
+    res.send(req.body.name);
+})
 
 app.listen(port, () => {
     console.log(`Server gestartet auf Port: ${port}`);
